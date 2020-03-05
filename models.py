5de33367faeb733f7b280674c85cff7e95896fc7
nafanyaxxx <ruslanr@live.ru>
2020-03-05 20:37:08 +0200

    foreign key example

--- models.py
+++ models.py
@@ -1,4 +1,7 @@
-from peewee import SqliteDatabase, Model, CharField
+from peewee import SqliteDatabase, Model, CharField, ForeignKeyField
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 db = SqliteDatabase('test.db')
 
@@ -8,5 +11,10 @@ class BaseModel(Model):
         database = db
 
 
+class List(BaseModel):
+    name = CharField()
+
+
 class Text(BaseModel):
+    list_ = ForeignKeyField(List, related_name='texts')
     text = CharField()
--- run.py
+++ run.py
@@ -1,22 +1,34 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QComboBox, QLineEdit
-from models import Text
+from models import Text, List, db
 
-Text.create_table()
+db.create_tables([Text, List])
 
 app = QApplication([])
 window = QWidget()
 layout = QVBoxLayout()
+new_list = QLineEdit()
 line_edit = QLineEdit()
 button1 = QPushButton('Add')
 button2 = QPushButton('Delete')
+add_list_btn = QPushButton('New List')
 
-combobox = QComboBox()
-texts = [t.text for t in Text.select()]
-combobox.addItems(texts)
+texts = QComboBox()
+lists = QComboBox()
 
+txts = [t.text for t in Text.select()]
+lists_models = [l for l in List.select()]
+if not lists_models:
+    lists_models.append(List.create(name='Default'))
+
+lists.addItems([l.name for l in lists_models])
+texts.addItems([txt.text for txt in lists_models[0].texts])
+
+layout.addWidget(lists)
+layout.addWidget(new_list)
+layout.addWidget(add_list_btn)
 layout.addWidget(line_edit)
 layout.addWidget(button1)
-layout.addWidget(combobox)
+layout.addWidget(texts)
 layout.addWidget(button2)
 
 window.setLayout(layout)
@@ -27,18 +39,39 @@ def on_button1_click():
     user_value = line_edit.text()
     if not user_value:
         return
-
-    Text.create(text=user_value)
-    combobox.addItem(user_value)
+    current_list = List.get(name=lists.currentText())
+    Text.create(text=user_value, list_=current_list)
+    texts.addItem(user_value)
 
 
 def on_button2_click():
-    selected = combobox.currentText()
-    idx = combobox.currentIndex()
-    Text.delete().where(Text.text == selected).execute()
-    combobox.removeItem(idx)
+    selected = texts.currentText()
+    idx = texts.currentIndex()
+    current_list = lists.currentText()
+    Text.delete().where(
+        (Text.text == selected) & (Text.list_.name == current_list) ).execute()
+    texts.removeItem(idx)
 
 
+def list_create():
+    new_list_name = new_list.text()
+    List.create(name=new_list_name)
+    lists.addItem(new_list_name)
+
+
+def on_select_list():
+    texts.clear()
+    list_name = lists.currentText()
+    list_model = List.get(name=list_name)
+    print('--------------')
+    for txt_model in list_model.texts:
+        texts.addItem(txt_model.text)
+    print('########')
+    # texts.addItems()
+
+
+lists.currentTextChanged.connect(on_select_list)
+add_list_btn.clicked.connect(list_create)
 button1.clicked.connect(on_button1_click)
 button2.clicked.connect(on_button2_click)
 app.exec()
